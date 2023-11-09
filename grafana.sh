4e2b35194742875a6bdee7924956c4c1b9ab6ada
Norffolk <91791661+Norffolk@users.noreply.github.com>
2023-11-09 10:53:32 -0300

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,14 @@
+#grafana.sh
+#!/bin/bash
+
+sudo apt-get install -y software-properties-common wget
+
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt update -y
+
+sudo apt install grafana -y
